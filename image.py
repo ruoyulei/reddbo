0be91afb61267f921cb7498a84bdbd587d60a905
xiang chen <xiangchen2016@gmail.com>
2017-05-27 11:44:53 -0700

    Fix bug

--- image.py
+++ image.py
@@ -14,7 +14,7 @@ def get_image_link(my_url):
     
     soup = BS(html)
     
-    temp = soup.find_all("img", class = "preview")
+    temp = soup.find_all("img", { "class" : "preview"})
     
     
     imge_links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', temp)
