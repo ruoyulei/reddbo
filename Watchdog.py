8947905db395e3d9027e1ddc5d8e196607fb8aef
ruoyulei@gmail.com <ruoyulei@gmail.com>
2017-06-19 20:54:13 -0700

    service restart watchdog added

---
+++ Watchdog.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+
+if __name__ == '__main__':
+	cmd = 'ps -ef'
+	proc = 'python main.py'
+	while 1:
+		flag = False
+		out = subprocess.check_output(cmd, shell=True)
+		line = out.splitlines()
+		for a in line:
+			if proc in a:
+				flag = True
+
+		if not flag:
+			time.sleep(60)
+		else:
+			os.system('python main.py &')
